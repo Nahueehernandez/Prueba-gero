3bbf644b48cca3f3409178da002af9e3b1b96485
Nahueehernandez <r44893426@gmail.com>
2022-04-11 11:58:35 -0300

    Update Ejercicio1.cs

--- Assets/Ejercicio1.cs
+++ Assets/Ejercicio1.cs
@@ -2,12 +2,34 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Ejercicio1 : MonoBehaviour
-{
+public class Ejercicio1 : MonoBehaviour { 
+    public string nahuelhernandez;
+    public int num1;
+    public string T;
+public int num2;
+    public int num3;
+    public int num4;
+    public int num5;
+
+
     // Start is called before the first frame update
     void Start()
     {
+        num1 = 1;
+        num2 = 2;
+        num3 = 3;
+        num4 = 4;
+        num5 = 5;
+
+        Debug.Log("Solo se puede ingresar T, D, G, M, H ");
+
+
+        if ( num3; num4; num5; <= num1; num2)
+            { Debug.Log("Error aun estas en ciclo basico");
+
+             }
         
+    
     }
 
     // Update is called once per frame
